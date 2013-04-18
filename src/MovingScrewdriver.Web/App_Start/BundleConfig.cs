7d202cb033f67c648efd9d8768d4490c57d3c2f7
Jakub Gutkowski <j.gutkowski@gmail.com>
2013-04-18 21:59:04 +0200

    adding cache to bundles

--- src/MovingScrewdriver.Web/App_Start/BundleConfig.cs
+++ src/MovingScrewdriver.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Optimization;
+using MovingScrewdriver.Web.Infrastructure;
 
 namespace MovingScrewdriver.Web
 {
@@ -9,35 +10,46 @@ namespace MovingScrewdriver.Web
         {
             bundles.UseCdn = true; 
             var jqueryCdn = "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js";
-            
-            bundles.Add(new ScriptBundle("~/bundles/jquery",
-                jqueryCdn).Include(
-                "~/Content/js/lib/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/libs").Include(
-                        "~/Content/js/lib/b*",
-                        "~/Content/js/lib/m*",
-                        "~/Content/js/lib/p*",
-                        //"~/Content/js/lib/rainbow.js",
-                        "~/Content/js/lib/jquery.pnotify.js",
-                        "~/Content/js/lib/prettify/prettify.js",
-                        "~/Content/js/ie10wp8.js",
-                        "~/Content/js/app.js"
-                        ));
+            var jq = new ScriptBundle("~/bundles/jquery",
+                                      jqueryCdn).Include(
+                                          "~/Content/js/lib/jquery-{version}.js");
+            jq.Transforms.Add(new CacheBundleTransform());
+            bundles.Add(jq);
+
+            var libs = new ScriptBundle("~/bundles/libs").Include(
+                "~/Content/js/lib/b*",
+                "~/Content/js/lib/m*",
+                "~/Content/js/lib/p*",
+                //"~/Content/js/lib/rainbow.js",
+                "~/Content/js/lib/jquery.pnotify.js",
+                "~/Content/js/lib/prettify/prettify.js",
+                "~/Content/js/ie10wp8.js",
+                "~/Content/js/app.js"
+                );
+            libs.Transforms.Add(new CacheBundleTransform());
+            bundles.Add(libs);
             
+
             bundles.Add(new ScriptBundle("~/bundles/social").Include(
                 "~/content/js/social.js"
                 ));
-            bundles.Add(new ScriptBundle("~/bundles/post-view").Include(
+
+            var post = new ScriptBundle("~/bundles/post-view").Include(
                 "~/content/js/views/post-view.js"
-                ));
+                );
+            post.Transforms.Add(new CacheBundleTransform());
+            bundles.Add(post);
+
             bundles.Add(new ScriptBundle("~/bundles/screw-on").Include(
                 "~/content/js/views/screw-on.js"
                 ));
 
-            bundles.Add(new StyleBundle("~/bundles/css").Include(
+            var css = new StyleBundle("~/bundles/css").Include(
                 "~/content/css/site.css"
-                ));
+                );
+            css.Transforms.Add(new CacheBundleTransform());
+            bundles.Add(css);
 
             //BundleTable.EnableOptimizations = true;
         }
---
+++ src/MovingScrewdriver.Web/Infrastructure/CacheBundleTransform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MovingScrewdriver.Web.Infrastructure
+{
+    public class CacheBundleTransform : IBundleTransform 
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            context.UseServerCache = true;
+            HttpCachePolicyBase cachePolicy = context.HttpContext.Response.Cache;
+            cachePolicy.SetCacheability(response.Cacheability);
+            cachePolicy.SetOmitVaryStar(true);
+            cachePolicy.SetExpires(ApplicationTime.Current.UtcDateTime.AddYears(1));
+            cachePolicy.SetValidUntilExpires(true);
+            cachePolicy.SetLastModified(ApplicationTime.Current.UtcDateTime);
+            cachePolicy.VaryByHeaders["User-Agent"] = true;
+        }
+    }
+}
